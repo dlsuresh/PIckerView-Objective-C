49524b857c63b8bb610bd506f4a7f9baf32c1117
dlsuresh <dl.suresh6@gmail.com>
2017-02-15 15:05:13 +0530

    Initial Commit

---
+++ PickerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PickerView
+//
+//  Created by Suresh on 2/15/17.
+//  Copyright © 2017 Suresh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
